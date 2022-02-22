d4b04214973c583c479295da1b0585a8b4c75a21
prithviraj31574984 <pg79@njit.edu>
2022-02-22 11:31:39 -0500

    m4

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -22,7 +22,7 @@ class MyCalc:
             return float(val)
         else:
             raise Exception("Not a number")
-# ucid=pg79 date=20/02/2022
+
     def add(self, num1, num2):
         if num1 == "ans":
             return self.add(self.ans, num2)
@@ -31,7 +31,7 @@ class MyCalc:
             num2 = self._as_number(num2)
             self.ans = num1+num2
         return self.ans
-# ucid=pg79 date=20/02/2022
+
     def sub(self, num1, num2):
         if num1 == "ans":
             return self.sub(self.ans, num2)
@@ -40,7 +40,7 @@ class MyCalc:
             num2 = self._as_number(num2)
             self.ans = num1-num2
         return self.ans
-# ucid=pg79 date=20/02/2022
+
     def mult(self, num1, num2):
         if num1 == "ans":
             return self.mult(self.ans, num2)
@@ -49,7 +49,7 @@ class MyCalc:
             num2 = self._as_number(num2)
             self.ans = num1*num2
         return self.ans
-# ucid=pg79 date=20/02/2022
+
     def div(self, num1, num2):
         if num1 == "ans":
             return self.div(self.ans, num2)
@@ -62,8 +62,40 @@ class MyCalc:
                 self.ans = num1/num2
         return self.ans
 
-# ucid=pg79 date=20/02/2022
-# ucid=pg79 date=20/02/2022
+
+if __name__ == '__main__':
+    is_running = True
+    iSTR = input("Are you ready?")
+    calc = MyCalc()
+    print(calc)
+    if iSTR == "yes":
+        while is_running:
+            iSTR = input("What is your eq:")
+            if iSTR == "quit" or iSTR == "q":
+                is_running = False
+            else:
+                print("Your eq was " + str(iSTR))
+                if "+" in iSTR:
+                    nums = iSTR.split("+")
+                    r = calc.add(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done before - check to hanlde negative values
+                elif "/" in iSTR:
+                    nums = iSTR.split("/")
+                    r = calc.div(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+                elif "*" in iSTR or "x" in iSTR:
+                    nums = iSTR.split("*") if "*" in iSTR else iSTR.split("x")
+                    r = calc.mult(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done last so negative numbers work
+                elif "-" in iSTR:
+                    nums = iSTR.split("-")
+                    r = calc.sub(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+
 
 
     else:
