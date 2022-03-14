02f034edc386be60d95fe93036eeba25b3c85639
prithviraj31574984 <pg79@njit.edu>
2022-03-14 13:12:47 -0500

    clac

--- M4/MyCalc.py
+++ M4/MyCalc.py
@@ -29,7 +29,7 @@ class MyCalc:
         else:
             num1 = self._as_number(num1)
             num2 = self._as_number(num2)
-            self.ans = num1+num2
+            self.ans = num1 + num2
         return self.ans
 
     def sub(self, num1, num2):
@@ -38,7 +38,7 @@ class MyCalc:
         else:
             num1 = self._as_number(num1)
             num2 = self._as_number(num2)
-            self.ans = num1-num2
+            self.ans = num1 - num2
         return self.ans
 
     def mult(self, num1, num2):
@@ -47,7 +47,7 @@ class MyCalc:
         else:
             num1 = self._as_number(num1)
             num2 = self._as_number(num2)
-            self.ans = num1*num2
+            self.ans = num1 * num2
         return self.ans
 
     def div(self, num1, num2):
@@ -59,8 +59,70 @@ class MyCalc:
             if num2 == 0:
                 print("Can't divide by zero, sorry")
             else:
-                self.ans = num1/num2
+                self.ans = num1 / num2
         return self.ans
+
+
+    def mean(L):
+        total = 0
+        for x in L:
+            total += x
+        mean = total / len(L)
+        return mean
+
+    def median(L):
+        L.sort()
+        if len(L) % 2 != 0:
+            median = L[int(len(L) / 2)]
+        else:
+            median = L[(int(len(L) / 2)) - 1] + L[int(len(L) / 2)]
+            median = median / 2
+        return median
+
+    def mode(L):
+        counter = 0
+        num = L[0]
+
+        for i in L:
+            curr_frequency = L.count(i)
+            if (curr_frequency > counter):
+                counter = curr_frequency
+                num = i
+            if len(set(L)) == len(L):
+                return 'there is no mode'
+
+        return num
+
+    def variance(data):
+        n = len(data)
+        mean = sum(data) / n
+        deviations = [(x - mean) ** 2 for x in data]
+        variance = sum(deviations) / n
+        return variance
+
+    def std_dev(ls):
+        n = len(ls)
+        mean = sum(ls) / n
+        var = sum((x - mean) ** 2 for x in ls) / n
+        std_dev = var ** 0.5
+        return std_dev
+
+    number_list = []
+
+    while (True):
+        ask = input('enter a number and say "stop" to end: ')
+        if ask == 'stop':
+            break
+        number_list.append(int(ask))
+
+    mean = str(mean(number_list))
+    median = str(median(number_list))
+    mode = str(mode(number_list))
+    variance = str(variance(number_list))
+    std_dev = str(std_dev(number_list))
+
+    print(
+        'mean: ' + mean + '\n' + 'median: ' + median + '\n' + 'mode: ' + mode + '\n' + 'variance: ' + variance + '\n' + 'std_dev: ' + std_dev + '\n')
 
 
 if __name__ == '__main__':
@@ -98,6 +160,9 @@ if __name__ == '__main__':
 
 
 
+
+
     else:
+
         print("Good bye")
         is_running = False
