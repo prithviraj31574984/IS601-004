28c7818846f88555fb9a3bb8f4d6f07db5ba8fbb
prithviraj31574984 <pg79@njit.edu>
2022-04-20 10:57:30 -0400

    adding files

---
+++ Project/helpers.py
@@ -0,0 +1,8 @@
+from flask import flash
+
+def handle_duplicate_column(error):
+    if "UNIQUE constraint" in error:
+        col = error.split(".")[1]
+        flash(f"{col.capitalize()} already exists, please try another", "warning")
+        return True
+    return False
